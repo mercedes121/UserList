ef500565bb9f16b9a0a19bf333b3684d8fe5463f
Suraj123 <suraj.satmohankar@gmail.com>
2025-04-30 10:03:53 +0530

    updated

---
+++ src/main/java/com/example/api/UserListApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserListApplication.class, args);
+	}
+
+}
